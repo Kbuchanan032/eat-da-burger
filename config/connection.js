ffc541f9c56e4ea5cf6e1b37441aa26f55c43986
Keith Buchanan <Kbuchanan77@yahoo.com>
2020-01-11 12:46:05 -0700

    Made a lot of progress here on this one. created the file strucutre. Also got the server up, the db built, the connections made, and the orms page up. Am now working on getting the helpers on the orms page.

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+var mysql = require("mysql");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Kbuchanan32",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  connection.end();
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+USE burgers_db;
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Bacon Cheese Burger", false);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Turkey Swiss Cheeseburger", false);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("southwest Blackbean Burger", false);

9bb3d362c46a440850d24841394fe68ca4ea2f70
Keith Buchanan <Kbuchanan77@yahoo.com>
2020-01-11 17:16:31 -0700

    burger_controller file is completed

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var burger = require("../models/burger");
+
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    var hdbrsObj = {
+      burgers: data
+    };
+    console.log(hdbrsObj);
+    res.render("index", hdbrsObj);
+  });
+
+  router.post("/api/burgers", function(req, res) {
+    burger.insertOne(
+      ["burger_name, devoured"],
+      [req.bod.burger_name, req.body.devoured],
+      function(result) {
+        res.json({ id: result.insertID });
+      }
+    );
+  });
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+    burger.updateOne({ devoured: req.body.devoured }, condition, function(
+      result
+    ) {
+      if ((reuslt, changeRows === 0)) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  router.deleteOne(condition, function(req, res) {
+    if ((reuslt, changeRows === 0)) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+module.exports = router;
